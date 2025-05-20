328b3c474ab221661710a820fa5d26c3c328e0f9
ZeroDayD <ZeroDayD@users.noreply.github.com>
2025-05-20 11:42:26 +0000

    deploy: 775bacb81b35b670e3a2b1d445777d5b2ad80122

--- utils/api-mock.helper.ts
+++
@@ -1,9 +0,0 @@
-import { Route } from '@playwright/test';
-
-export async function fulfillJson(route: Route, body: any, status = 200) {
-    await route.fulfill({
-        status,
-        contentType: 'application/json',
-        body: JSON.stringify(body),
-    });
-}
